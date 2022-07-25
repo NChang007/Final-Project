11d6be688386dae13a6501a4965668e8bc81f0ad
ilorlando10 <orlandolife@hotmail.com>
2022-07-25 23:51:52 +0000

    new update

--- src/front/js/component/Card.js
+++ src/front/js/component/Card.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+ //anything in comment
 
 function Card() {
   return (
